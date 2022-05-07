2ef83e0d5c12caabdb90aa6b36b2b20116c58864
djfdmator <djfdmator@naver.com>
2022-05-07 14:01:53 +0900

    UI Automation

---
+++ Inflearn_MMO/Assets/Scripts/UI/UI_Button.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UI_Button : UI_Base
+{
+    enum Buttons
+    {
+        PointButton
+    }
+
+    enum Texts
+    {
+        PointText,
+        ScoreText
+    }
+
+    enum GameObjects
+    {
+        TestObject
+    }
+
+    void Start()
+    {
+        Bind<Button>(typeof(Buttons));
+        Bind<Text>(typeof(Texts));
+        Bind<GameObject>(typeof(GameObjects));
+
+        Get<Text>((int)Texts.ScoreText).text = "BindText";
+
+        Debug.Log(Get<GameObject>((int)GameObjects.TestObject).name);
+    }
+
+    void Update()
+    {
+
+    }
+}
